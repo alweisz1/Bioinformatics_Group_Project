4cfa91e7d2d6368629d612eb9b34af911c0cb312
alweisz1 <awei1@nd.edu>
2018-12-07 23:13:52 -0500

    AHHHH. Some updates and comments

--- ToSubmit/Pt6_FilesSripts/Pt3_HMM/hmmscript.sh
+++ ToSubmit/Pt6_FilesSripts/Pt3_HMM/hmmscript.sh
@@ -1,4 +1,4 @@
-# iterate over files ending in *align.fasta
+ # iterate over files ending in *align.fasta
 for filename in *align.fasta
 do
     #make a variable of the same name except it ends it ".hmm"
